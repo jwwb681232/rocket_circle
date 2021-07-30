13e025672a6801a95d8ac1cac4c544c756a21dcf
wangxiao <wangxiao@guadoutech.com>
2021-07-30 15:43:31 +0800

    update index

--- src/actions/user_action.rs
+++ src/actions/user_action.rs
@@ -1,10 +1,28 @@
-use chrono::{NaiveDateTime, Utc};
+use chrono::{NaiveDateTime, Utc, Local, TimeZone};
 use crate::bootstrap::database::Db;
 use rocket::form::Form;
 use rocket::serde::json::Json;
 use serde::Serialize;
 use crate::schema::users;
 use diesel::prelude::*;
+use crate::models::user::UserIndex;
+use crate::WebResult;
+
+#[get("/")]
+pub async fn index(db: Db) -> WebResult<Json<Vec<UserIndex>>> {
+
+    let results: Vec<UserIndex> = db.run(move |conn| {
+        crate::schema::users::dsl::users.limit(15).load::<UserIndex>(conn)
+    }).await?;
+
+    for result in &results {
+        println!("{:#?}", Local.timestamp(result.created_at.timestamp(), 0).to_rfc2822().to_string());
+        println!("{:#?}", Local.timestamp(result.created_at.timestamp(), 0).to_rfc3339().to_string());
+        println!("{:#?}", Local.timestamp(result.created_at.timestamp(), 0).format("%Y-%m-%d %H:%M:%S").to_string());
+    }
+
+    Ok(Json(results))
+}
 
 
 #[derive(Serialize)]
@@ -14,11 +32,12 @@ struct StoreResponse {
     message: String,
 }
 
-#[derive(Debug, FromForm)]
+#[derive(Debug, FromForm,Clone)]
 pub struct StoreRequest {
     name: String,
     email: String,
     password: String,
+    #[field(validate = eq(self.password.clone()))]
     password_confirm: String,
 }
 
@@ -35,15 +54,16 @@ struct NewUser {
 
 #[post("/", data = "<request>")]
 pub async fn store(db: Db, request: Form<StoreRequest>) -> Json<StoreResponse> {
-    println!("{:?}", request);
+
+    let req = request.clone();
 
     let result: usize = db.run(move |conn| {
 
         let new_user = NewUser{
-            name: request.name.parse().unwrap(),
-            email: request.email.parse().unwrap(),
+            name: req.name,
+            email: req.email,
             email_verified_at: None,
-            password:  request.password.to_string(),
+            password:  req.password,
             created_at: Utc::now().naive_local(),
             updated_at: Utc::now().naive_local(),
         };
